4a941a32bb5e6594a201c2136714757f37eeb573
Lucas Rauh <47648082+termilu@users.noreply.github.com>
2025-05-15 17:02:05 +0200

    Added polymer resourcepack dependency

--- src/main/java/com/termilu/cosmetictrims/item/PolymerBackedItem.java
+++ src/main/java/com/termilu/cosmetictrims/item/PolymerBackedItem.java
@@ -1,19 +1,37 @@
 package com.termilu.cosmetictrims.item;
 
 import eu.pb4.polymer.core.api.item.PolymerItem;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.CustomModelDataComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.tooltip.TooltipType;
 import xyz.nucleoid.packettweaker.PacketContext;
+import java.util.List;
 
 public class PolymerBackedItem extends Item implements PolymerItem {
 
     public PolymerBackedItem(Settings settings) {
         super(settings);
+
+
     }
 
     @Override
     public Item getPolymerItem(ItemStack stack, PacketContext context) {
-        return Items.CHAINMAIL_HELMET;
+        return Items.LEATHER_HELMET;
     }
+
+//    @Override
+//    public ItemStack getPolymerItemStack(ItemStack original, TooltipType tooltipType, PacketContext context) {
+//        ItemStack stack = new ItemStack(getPolymerItem(original, context));
+//        stack.setCount(original.getCount());
+//
+//
+//        stack.set(DataComponentTypes.CUSTOM_MODEL_DATA, new CustomModelDataComponent());
+//
+//        return stack;
+//    }
+
 }
